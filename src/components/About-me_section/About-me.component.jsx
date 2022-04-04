f363dfe22411982f235fcf41bb99e45097c18246
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-04 11:53:56 -0300

    Redimensionamento de Sobre Mim

--- src/components/About-me_section/About-me.component.jsx
+++ src/components/About-me_section/About-me.component.jsx
@@ -3,27 +3,33 @@ import portfolioAuthor from "../../assets/imgs/profile/felipe-miranda-marreiros.
 
 const AboutMe = () => {
   return (
-    <>
-      <div className="about-me-section container" id="Sobre Mim">
-        <section className="about-me-section__content">
-          <div>
-            <img
-              src={portfolioAuthor}
-              alt="Um foto de perfil com fundo branco"
-              className="about-me-section__content-img"
-            />
-          </div>
-          <div className="about-me-section__content-text">
-            <h2 className="about-me-section__content-text--title">Sobre Mim</h2>
-            <p>
-              Meu nome é Felipe Miranda Marreiros, tenho 24 anos e sou formado
-              em Gestão de Recursos Humanos pelo Instituto Federal de Ciência e
-              Tecnologia do Amapá.
-            </p>
-          </div>
-        </section>
-      </div>
-    </>
+    <div className="about-me-section container" id="Sobre Mim">
+      <section className="about-me-section__content">
+        <div>
+          <img
+            src={portfolioAuthor}
+            alt="Um foto de perfil com fundo branco"
+            className="about-me-section__content-img"
+          />
+        </div>
+        <div className="about-me-section__content-text">
+          <h2 className="about-me-section__content-text--title">Sobre Mim</h2>
+          <p>
+            Meu nome é Felipe Miranda Marreiros, tenho 24 anos e sou formado em
+            Gestão de Recursos Humanos pelo Instituto Federal de Ciência e
+            Tecnologia do Amapá.
+          </p>
+          <p>
+            Comecei estudar programação em outubro de 2021 e hoje faço parte do
+            programa Oracle Next Education.
+          </p>
+          <p>
+            Interessei-me por Front-End principalmente pela facilidade na curva
+            de aprendizagem e por ter gostado de CSS.
+          </p>
+        </div>
+      </section>
+    </div>
   );
 };
 
