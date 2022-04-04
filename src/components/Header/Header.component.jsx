5bb86344596036ac43ec6c68a70593126608690d
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-04 01:06:39 -0300

    Hero componente

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.style.scss";
+import heroIllustration from "../../assets/imgs/svg/hero-vector-illustration.svg";
+
+const Header = () => {
+  return (
+    <header className="hero">
+      <div className="container">
+        <div className="hero__content">
+          <section className="hero__content-text">
+            <h1 className="hero__content-text--title">
+              Olá, meu nome é{" "}
+              <span className="hero__content-text--name">Felipe Miranda</span>
+            </h1>
+            <p>
+              Seja bem-vindo ao meu portfolio. Todos os meus projetos estão
+              presentes aqui, bem como minha formação e o que estou estudando
+              atualmente.
+            </p>
+            <button className="hero-btn">Saiba Mais</button>
+          </section>
+          <div className="hero__content-img">
+            <img
+              src={heroIllustration}
+              alt="Felipe Miranda Marreiros"
+              title="Felipe Miranda Marreiros"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
