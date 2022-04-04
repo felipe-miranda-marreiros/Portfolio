c9c6e36f079fbbb95839d63ae848cc1449e98847
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-04 01:06:51 -0300

    Navbar componente

---
+++ src/components/Navbar/Navbar.component.jsx
@@ -0,0 +1,34 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Navbar.style.scss";
+
+const Navbar = () => {
+  const navbarLinks = ["Sobre Mim", "Skills", "Projetos", "Contato"];
+  const navBarElements = navbarLinks.map((links, i) => {
+    return (
+      <li key={i}>
+        <a href={`#${links}`}>{links}</a>
+      </li>
+    );
+  });
+
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("showNavbar");
+  };
+
+  return (
+    <nav className="navbar">
+      <span className="logo">Felipe Miranda</span>
+      <ul className="navbar__items" ref={navRef}>
+        {navBarElements}
+      </ul>
+      <button className="navbar__btns" onClick={showNavbar}>
+        <FaTimes className="navbar__menu" name="close-outline" />
+        <FaBars className="navbar__menu" />
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
